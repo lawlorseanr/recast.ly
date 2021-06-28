0fdf0b024da31f23702752cdc1b53952af5439ee
Kenneth Hsu <kenneth@keihsu.dev>
2021-06-28 15:44:05 -0700

    Imported VideoList & VideoPlayer to App Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,13 @@
-// var App = () => (
-//   <div>
-//     <nav className="navbar">
-//       <div className="col-md-6 offset-md-3">
-//         <div><h5><em>search</em> view goes here</h5></div>
-//       </div>
-//     </nav>
-//     <div className="row">
-//       <div className="col-md-7">
-//         <div><h5><em>videoPlayer</em> view goes here</h5></div>
-//       </div>
-//       <div className="col-md-5">
-//         <div><h5><em>videoList</em> view goes here</h5></div>
-//       </div>
-//     </div>
-//   </div>
-// );
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
 
 class App extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      currentVideo: props.videos[0],
+      videos: props.videos,
+    };
   }
   render () {
     return (
@@ -30,10 +19,10 @@ class App extends React.Component {
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em>videoPlayer</em> view goes here</h5></div>
+            <VideoPlayer video={this.state.currentVideo}/>
           </div>
           <div className="col-md-5">
-            <div><h5><em>videoList</em> view goes here</h5></div>
+            <VideoList videos={this.state.videos.splice(1)}/>
           </div>
         </div>
       </div>
