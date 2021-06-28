0bff4be2620133b68875304842de29dbc0eb7098
Kenneth Hsu <kenneth@keihsu.dev>
2021-06-28 16:56:55 -0700

    Implemented searchYouTube method to GET query

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,7 +1,7 @@
 var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" onClick={() => { props.handleClick(props.video); }}/>
     </div>
     <div className="media-body">
       <div className="video-list-entry-title" onClick={() => { props.handleClick(props.video); }}>{props.video.snippet.title}</div>
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,11 +1,16 @@
 import { API_KEY, YOUTUBE_API_KEY } from '../config/config.js';
 
+const SEARCH_ENDPOINT = 'https://app-hrsei-api.herokuapp.com/api/recastly/videos';
+
 $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
 var searchYouTube = (query, callback) => {
   // TODO
+  $.get(SEARCH_ENDPOINT,
+    {q: query},
+    data => { callback(data); });
 };
 
 export default searchYouTube;
